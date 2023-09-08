a1c134a3ad82863ea1c07c9d2e7229bc4f7dc8a4
Priscila Schleske <plasch7@gmail.com>
2023-09-08 12:18:43 -0600

    Adds MainNav styles

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,21 +1,46 @@
+import Container from 'react-bootstrap/Container';
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+
+
 export default function MainNav({ brand, leftLinks, rightLinks }) {
   return (
-    <nav>
-      <a href="/">{brand}</a>
-      <ul>
-        {leftLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {rightLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <NavBar expand="lg" className="bg-success navbar-dark">
+      <Container fluid>
+        <NavBar.Brand href="/">{brand}</NavBar.Brand>
+        <NavBar.Toggle />
+        <NavBar.Collapse>
+          <Nav className='me-auto'>
+            {leftLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>{text}</Nav.Link>
+            ))}
+          </Nav>
+          <Nav>
+            {rightLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>{text}</Nav.Link>
+            ))}
+          </Nav>
+        </NavBar.Collapse>
+      </Container>
+    </NavBar>
   );
+
+  //   <nav>
+  //     <a href="/">{brand}</a>
+  //     <ul>
+  //       {leftLinks.map(({ url, text }) => (
+  //         <li key={text}>
+  //           <a href={url}>{text}</a>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //     <ul>
+  //       {rightLinks.map(({ url, text }) => (
+  //         <li key={text}>
+  //           <a href={url}>{text}</a>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </nav>
+  // );
 }
