bf4a4d4468906bc9011bc8bd5c6986a8d5b6f4f1
Priscila Schleske <plasch7@gmail.com>
2023-09-08 13:09:11 -0600

    fixes centering, mx-auto NOT max-auto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import { Col, Row, Container } from 'react-bootstrap';
 
 export default function Hero({ title, lead }) {
   return (
-    <Container fluid className='text-center py-5 bg-light'>
+    <Container fluid className='bg-light text-center py-5'>
       <Row className='py-lg-5'>
-        <Col lg='6' md='8' className='max-auto'>
+        <Col lg='6' md='8' className='mx-auto'>
           <h1 className='fw-light'>{title}</h1>
           <p className='lead text-muted'>{lead}</p>
         </Col>
