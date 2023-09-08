d68e33f99b03504829cd0bd8f234db0e566f0ac2
Priscila Schleske <plasch7@gmail.com>
2023-09-08 13:06:14 -0600

    styles values

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,19 +1,37 @@
+import { Card, Row, Col, Container, Button } from 'react-bootstrap';
+
 function Value({ title, description, action }) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>{action}</p>
-    </div>
+    <Card className='shadow-sm h-100'>
+      <Card.Body className='p-4 d-flex flex-column'>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant='success' className='mt-auto'>{action}</Button>
+      </Card.Body>
+    </Card>
+    // <div>
+    //   <h2>{title}</h2>
+    //   <p>{description}</p>
+    //   <p>{action}</p>
+    // </div>
   );
 }
 
 export default function Values({ values }) {
   return (
-    <section>
-      {values.map(({ title, description, action }, index) => (
-        <Value key={index} title={title} description={description} action={action} />
-      ))}
-    </section>
+    <Container className='py-5'>
+      <Row>
+        {values.map(({ title, description, action }, index) => (
+          <Col key={title}>
+            <Value title={title} description={description} action={action} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+    // <section>
+    //   {values.map(({ title, description, action }, index) => (
+    //     <Value key={index} title={title} description={description} action={action} />
+    //   ))}
+    // </section>
   );
 }
